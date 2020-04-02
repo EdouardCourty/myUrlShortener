961c83f49b7a62f6b00d259a75680cac5ee0cd51
Edouard Courty <edouard.courty2@gmail.com>
2020-04-02 21:32:28 +0200

    Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,14 +4,27 @@ module.exports = {
       name: "myUrlShortener",
       script: "bin/www",
       watch: true,
+      autorestart: true,
+      max_memory_restart: "500M",
       env: {
-        "PORT": 3000,
+        "APPLICATION_PORT": 3000,
         "NODE_ENV": "development"
       },
       env_production: {
-        "PORT": 3001,
+        "APPLICATION_PORT": 3001,
         "NODE_ENV": "production",
       }
     }
-  ]
-}
+  ],
+
+  deploy: {
+    production: {
+      user: "edouard",
+      host: "edouardcourty.fr",
+      ref: "origin/master",
+      path: "/var/www/myUrlShortener",
+      repo: "https://github.com/EdouardCourty/myUrlShortener",
+      "post-reploy": "npm i && pm2 reload ecosystem.config.js --env production"
+    }
+  }
+};
