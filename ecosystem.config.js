e4bcab30714135e4a99c17e600b29f3696f8c3b7
Edouard Courty <edouard.courty2@gmail.com>
2020-04-02 21:35:47 +0200

    Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,11 +8,15 @@ module.exports = {
       max_memory_restart: "500M",
       env: {
         "APPLICATION_PORT": 3000,
-        "NODE_ENV": "development"
+        "NODE_ENV": "development",
+        "UNIQUE_STRINGS_LENGTH": 7,
+        "HOSTNAME": "http://localhost:3000"
       },
       env_production: {
         "APPLICATION_PORT": 3001,
         "NODE_ENV": "production",
+        "UNIQUE_STRINGS_LENGTH": 7,
+        "HOSTNAME": "https://link.edouardcourty.fr"
       }
     }
   ],
