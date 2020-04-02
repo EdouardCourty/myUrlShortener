156111ec82bc66c742319838fdbe4992920964a7
Edouard Courty <edouard.courty2@gmail.com>
2020-04-02 21:23:54 +0200

    Update dependencies

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,37 +1,17 @@
 module.exports = {
-  apps : [{
-    name: 'UrlShortener',
-    script: './bin/www',
-
-    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    // args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '500M',
-    env: {
-      NODE_ENV: 'dev',
-      HOSTNAME: "http://localhost:3000",
-      APPLICATION_PORT: 3000,
-      UNIQUE_STRINGS_LENGTH: 7
-    },
-    env_production: {
-      NODE_ENV: 'prod',
-      HOSTNAME: "http://link.edouardcourty.fr",
-      APPLICATION_PORT: 3001,
-      IS_PRODUCTION: true,
-      UNIQUE_STRINGS_LENGTH: 7
+  apps : [
+    {
+      name: "myUrlShortener",
+      script: "bin/www",
+      watch: true,
+      env: {
+        "PORT": 3000,
+        "NODE_ENV": "development"
+      },
+      env_production: {
+        "PORT": 3001,
+        "NODE_ENV": "production",
+      }
     }
-  }],
-
-  deploy : {
-    production : {
-      user : 'edouard',
-      host : 'edouardcourty.fr',
-      ref  : 'origin/master',
-      repo : 'http://github.com/EdouardCourty/myUrlShortener',
-      path : '/var/www/link.edouardcourty.fr',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
-};
+  ]
+}
